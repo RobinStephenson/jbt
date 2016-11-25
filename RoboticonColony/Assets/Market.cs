1bb2530114e95b050934d206a23111e0086884b7
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2016-11-25 09:16:11 +0000

    Worked on market class a bit more

--- RoboticonColony/Assets/Market.cs
+++ RoboticonColony/Assets/Market.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Market : MonoBehaviour {
+public class Market {
+    public Resources Stock { get; private set; }
+    public bool Open { get; private set; }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Dictionary<TradeableItem, int> _buyPrices;
+    private Dictionary<TradeableItem, int> _sellPrices;
+
+    public Market(Resources stock)
+    {
+        Stock = stock;
+        Open = true;
+        //How will we populate buy and sell prices at the start of the game? Read from a text file? Randomly generate? Pass in?
+    }
+
+    public Market(Resources stock, bool open)
+    {
+        Stock = stock;
+        Open = open;
+    }
+
+    public int GetBuyPrice(TradeableItem item)
+    {
+        return _buyPrices[item];
+    }
+
+    public int GetSellPrice(TradeableItem item)
+    {
+        return _sellPrices[item];
+    }
+
+    public bool Buy(TradeableItem item, int Quantity)
+    {
+        return true;
+    }
 }
+
+public enum TradeableItem
+{
+    Ore = 1,
+    Power,
+    Roboticon
---
+++ RoboticonColony/Assets/Resources.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Resources {
+    public int money;
+    public int ore;
+    public int power;
+    public int roboticon;
+}
