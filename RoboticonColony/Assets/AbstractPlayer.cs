a80553cacf14ffe62152c21b50d9c4193099e251
Robin Stephenson <robin.mascros@gmail.com>
2016-11-25 11:15:11 +0000

    Created AbstractPlayer

---
+++ RoboticonColony/Assets/AbstractPlayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public abstract class AbstractPlayer
+{
+    private List<Tile> ownedTiles;
+    private Inventory inventory;
+
+    public abstract void DoPhase(int phase);
+}
