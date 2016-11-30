ae9a439bf02d9a6e17b45203cc5f48c37b984983
Robin Stephenson <robin.mascros@gmail.com>
2016-11-30 10:44:30 +0000

    Split do phase into multiple lines and created constructor

--- RoboticonColony/Assets/AbstractPlayer.cs
+++ RoboticonColony/Assets/AbstractPlayer.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public abstract class AbstractPlayer
 {
     private List<Tile> ownedTiles;
     private Inventory inventory;
+    private IInputController input;
 
-    public abstract void DoPhase(int phase);
+    public abstract void DoPhaseOne();
+    public abstract void DoPhaseTwo();
+    public abstract void DoPhaseThree();
+    public abstract void DoPhaseFour();
+    public abstract void DoPhaseFive();
+
+    protected AbstractPlayer(IInputController inputController)
+    {
+        input = inputController;
+    }
 }
