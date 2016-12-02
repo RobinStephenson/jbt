631d2f635a35d07ecc3b51d6421359b7c806343f
Tom <Tom@LAPTOP-EFR71R73>
2016-12-02 12:38:41 +0000

    Almost complete tile class with some practice grid layout on unity

---
+++ RoboticonColony/Assets/PhysicalTile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysalTile : MonoBehaviour {
+
+    private Tile tile;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- RoboticonColony/Assets/Tile.cs
+++ RoboticonColony/Assets/Tile.cs
@@ -1,15 +1,92 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-public class Tile : MonoBehaviour {
+sealed public class Tile
+{
+    //private Resources _resources;
+    //private Roboticon _installedRoboticon;
+    private int _cost;
+    private int _ownerID;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    /// <summary>
+        /// Constructor for tile with provided parameters
+        /// </summary>
+        /// <param name="resources">Resources that the tile has</param>
+        /// <param name="roboticon">Roboticon on the tile, null if no roboticon</param>
+        /// <param name="cost">Cost of the tile</param>
+        /// <param name="owner">Owner of the tile</param>
+    //public Tile(Resources resources, Roboticon roboticon, int cost, Player owner)
+    //{
+    //    _resources = resources;
+    //    _installedRoboticon = roboticon;
+    //    _cost = cost;
+    //    _ownerID = owner.PlayerID;
+    //}
+
+    /// <summary>
+        /// Constructor for tile with no owner or roboticon
+        /// </summary>
+        /// <param name="resource">Resources that the tile has</param>
+        /// <param name="cost">Cost of the tile</param>
+    //public Tile(Resources resource, int cost)
+    //{
+    //    _resources = resource;
+    //    _cost = cost;
+    //    //_installedRoboticon = null;
+    //    _ownerID = -1;
+    //}
+
+    /// <summary>
+        /// Adds a roboticon to the tile
+        /// </summary>
+        /// <param name="newRoboticon">Roboticon to be added</param>
+    //public void addRoboticon(Roboticon newRoboticon)
+    //{
+    //    _installedRoboticon = newRoboticon;
+    //}
+
+    /// <summary>
+        /// Removes the roboticon from the tile
+        /// </summary>
+    //public void removeRoboticon()
+    //{
+    //    _installedRoboticon = null;
+    //}
+
+    /// <summary>
+        /// Gets the cost of the tile
+        /// </summary>
+        /// <returns></returns>
+    public int getCost()
+    {
+        return _cost;
+    }
+
+    //public void produce()
+  
+    //}
+
+    //public Resources TileResources
+    //{
+    //    get { return _resources; }
+    //}
+
+    //public Roboticon InstalledRoboticon
+    //{
+    //    get { return _installedRoboticon; }
+    //    set { _installedRoboticon = value; }
+    //}
+
+    public int Cost
+    {
+        get { return _cost; }
+    }
+
+    public int OwnerID
+    {
+        get { return _ownerID; }
+        set { _ownerID = value; }
+    }
 }
