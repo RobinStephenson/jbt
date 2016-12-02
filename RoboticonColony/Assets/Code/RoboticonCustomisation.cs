f4f2e704b95852cd1abe7c3fcf00fb2353a87707
Matthew Turner <Juppaware@gmail.com>
2016-12-02 20:58:00 +0000

    Add Roboticon Customisation Prerequisit check

--- RoboticonColony/Assets/Code/RoboticonCustomisation.cs
+++ RoboticonColony/Assets/Code/RoboticonCustomisation.cs
@@ -24,7 +24,35 @@ public class RoboticonCustomisation : MonoBehaviour {
         Resource = resource;
     }
 
+    /// <summary>
+    /// Tests to see if the roboticon has met the requriements of the customisation. 
+    /// </summary>
+    /// <param name="CompletedCustomisations"> A list of all the completed customisations on the roboticon. </param>
+    /// <returns></returns>
+    public bool RequirementsMetTest(List<RoboticonCustomisation> CompletedCustomisations)
+    {
+        // If the customisation has Prerequisites then...
+        if (Prerequisites.Count > 0)
+        {
+            foreach (RoboticonCustomisation current_customisation in Prerequisites)
+            {
+                if (!CompletedCustomisations.Contains(current_customisation))
+                {
+                    // if Completed Customisations does not contains one Prerequisites then requirements are not met
+                    return false;
+                }
+            }
+            // if Completed Customisations contains all Prerequisites then requirements are met
+            return true;
+        }
+        // else (Customisation has no Prerequisites)
+        else
+        {
+            // As the customisation requires no prerequisites then the requirements are met
+            return true;
+        }
 
+    }
 
 
 }
