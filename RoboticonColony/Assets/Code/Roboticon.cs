7d198f7c7a8ddd8cf762d8ddeaee35f24261cdd1
Matthew Turner <Juppaware@gmail.com>
2016-12-02 18:20:36 +0000

    Creation of initialisation of roboticon
    Created the initialisation of roboticon where a tile is given to the
    class, and the bonus production parameters are set to base roboticon.

--- RoboticonColony/Assets/Code/Roboticon.cs
+++ RoboticonColony/Assets/Code/Roboticon.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 /// <summary>
 /// A Roboticon. Holds all information about the roboticon and facilitates upgrading said roboticon. 
 /// </summary>
-public class Roboticon : MonoBehaviour {
+sealed public class Roboticon : MonoBehaviour {
+
+    private Tile Current_tile;
 
     /// <summary>
     /// BonusProduction is a dictionary which holds the int value (Total production of each recource) it produces.
     /// </summary>
     public Dictionary<ItemType, int> BonusProduction;
 
+    /// <summary>
+    /// Creates a Roboticon instance that creates a non-customised roboticon in the specified tile.
+    /// </summary>
+    /// <param name="selected_tile"> The Tile that the roboticon is being placed on. </param>
+    public Roboticon(Tile selected_tile)
+    {
+        Current_tile = selected_tile;
+        BonusProduction = new Dictionary<ItemType, int>();
+        BonusProduction[ItemType.Ore] = 0;
+        BonusProduction[ItemType.Power] = 0;
+    }
    
 }
