26ac2367abd61b42cbfeb825f671276bc61b8535
Matthew Turner <Juppaware@gmail.com>
2016-12-02 17:54:36 +0000

    Roboticon Class creation
    Created the roboticon class and added BonusProduction dictionary

---
+++ RoboticonColony/Assets/Code/Roboticon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A Roboticon. Holds all information about the roboticon and facilitates upgrading said roboticon. 
+/// </summary>
+public class Roboticon : MonoBehaviour {
+
+    /// <summary>
+    /// BonusProduction is a dictionary which holds the int value (Total production of each recource) it produces.
+    /// </summary>
+    public Dictionary<ItemType, int> BonusProduction;
+
+   
+}
