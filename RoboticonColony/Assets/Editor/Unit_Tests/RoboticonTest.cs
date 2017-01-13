174c5b54bdd5b9dabf75de4bb8b69e9aa967f443
Matthew Turner <Juppaware@gmail.com>
2017-01-13 14:36:10 +0000

    Added more tests

--- RoboticonColony/Assets/Editor/Unit_Tests/RoboticonTest.cs
+++ RoboticonColony/Assets/Editor/Unit_Tests/RoboticonTest.cs
@@ -3,33 +3,42 @@ using System;
 using UnityEditor;
 using NUnit.Framework;
 using NUnit;
+using System.Collections.Generic;
 
 public class RoboticonTest
 {
     [Test]
     public void CreateNewRoboticon()
     {
-        Tile current_tile = new Tile();
+
         //Create an empty Roboticon instance
+        Tile current_tile = new Tile();
         Roboticon NewRoboticon = new Roboticon(current_tile);
 
-        //Check if all resources have initial value set to zero
-        Assert.AreEqual(0, NewRoboticon.BonusProductionGetter(ItemType.Ore));
-        Assert.AreEqual(0, NewRoboticon.BonusProductionGetter(ItemType.Power));
-        Assert.AreEqual(0, NewRoboticon.CurrentCustomisationGetter().Count);
+        //Check if all resources have initial value set to zero and lists are empty.
+        Assert.AreEqual(1, NewRoboticon.BonusProductionGetter(ItemType.Ore));
+        Assert.AreEqual(1, NewRoboticon.BonusProductionGetter(ItemType.Power));
+        Assert.AreEqual(0, NewRoboticon.CurrentCustomisations.Count);
     }
 
     [Test]
     public void SuccessfulCustomiseRoboticon()
     {
-        //Create a populated inventory instance
-        Inventory inv = new Inventory(4, 3, 2, 1);
+        //Create a new RoboticonCustomisation instance
+        List<RoboticonCustomisation> prelist = new List<RoboticonCustomisation>();
+        RoboticonCustomisation NewCustomisation = new RoboticonCustomisation("test", 2, prelist, ItemType.Ore);
+
+        //Create an empty Roboticon instance
+        Tile current_tile = new Tile();
+        Roboticon NewRoboticon = new Roboticon(current_tile);
+
+        //Customise the empty roboticon
+        NewRoboticon.CustomiseRoboticon(NewCustomisation);
 
         //Check if all resources have the value they were set
-        Assert.AreEqual(4, inv.Money);
-        Assert.AreEqual(3, inv.GetItemAmount(ItemType.Ore));
-        Assert.AreEqual(2, inv.GetItemAmount(ItemType.Power));
-        Assert.AreEqual(1, inv.GetItemAmount(ItemType.Roboticon));
+        Assert.AreEqual(2, NewRoboticon.BonusProductionGetter(ItemType.Ore));
+        Assert.AreEqual(2, NewRoboticon.BonusProductionGetter(ItemType.Power));
+        Assert.AreEqual(prelist, NewRoboticon.CurrentCustomisations);
     }
 
     [Test]
