6edb18b3c323ce414e2c69a4f406140561bf6c9f
Matthew Turner <Juppaware@gmail.com>
2017-01-14 22:27:07 +0000

    Created initial Roboticon Factory

---
+++ RoboticonColony/Assets/Code/RoboticonFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Roboticon Factory. Handles interactions between all roboticons and the player / market.  
+/// </summary>
+sealed class RoboticonFactory
+{
+
+    /// <summary>
+    /// customisationsList is a list of all RoboticonCustomisations
+    /// </summary>
+    private List<RoboticonCustomisation> customisationsList;
+
+    /// <summary>
+    /// roboticonList is a list of all built Roboticons
+    /// </summary>
+    private List<Roboticon> roboticonList;
+
+    public RoboticonFactory()
+    {
+        customisationsList = null;
+        roboticonList = null;
+
+    }
+
+}
