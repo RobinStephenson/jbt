415105dc9f5f90dfc3c58368db44f44522beabe3
Robin Stephenson <robin.mascros@gmail.com>
2017-01-14 21:34:03 +0000

    Made all timeouts in InputController nullable and gave ChooseTile cancelable as an option

--- RoboticonColony/Assets/Code/InputController.cs
+++ RoboticonColony/Assets/Code/InputController.cs
@@ -24,7 +24,7 @@ public class InputController : MonoBehaviour {
     /// <param name="timeout">the time the user has to decide</param>
     /// <exception cref="TimeoutException">the user has taken too long</exception>
     /// <returns>true or false based on the users selection</returns>
-    public bool PromptBool(string question, Timeout timeout, string trueOption = "Yes", string falseOption = "No")
+    public bool PromptBool(string question, Timeout timeout = null, string trueOption = "Yes", string falseOption = "No")
     {
         throw new TimeoutException();
     }
@@ -40,7 +40,7 @@ public class InputController : MonoBehaviour {
     /// <exception cref="CancelledException">the user cancelled the prompt</exception>
     /// <exception cref="TimeoutException">the user has taken too long</exception>
     /// <returns>the chosen value</returns>
-    public int PromptInt(string question, Timeout timeout, int min = 0, int max = int.MaxValue, bool cancelable = false)
+    public int PromptInt(string question, Timeout timeout = null, int min = 0, int max = int.MaxValue, bool cancelable = false)
     {
         throw new CancelledException();
     }
@@ -50,13 +50,13 @@ public class InputController : MonoBehaviour {
     /// </summary>
     /// <typeparam name="T">The type of the list of items which must inherit AbstractPromptListItem</typeparam>
     /// <param name="question">the questions to display to the user</param>
-    /// <param name="timeout">the time the user has to decide</param>
     /// <param name="items">the list of items the user can choose from.</param>
+    /// <param name="timeout">the time the user has to decide</param>
     /// <param name="cancelable">should teh user be able to cancel the prompt</param>
     /// <exception cref="CancelledException">the user cancelled the prompt</exception>
     /// <exception cref="TimeoutException">the user has taken too long</exception>
     /// <returns>the selected item</returns>
-    public T PromptList<T>(string question, Timeout timeout, List<T> items, bool cancelable = false) where T: AbstractPromptListItem
+    public T PromptList<T>(string question, List<T> items, Timeout timeout = null, bool cancelable = false) where T: AbstractPromptListItem
     {
         throw new CancelledException();
     }
@@ -67,7 +67,7 @@ public class InputController : MonoBehaviour {
     /// <param name="message">the message for the user to read</param>
     /// <param name="timeout">how long the user has</param>
     /// <exception cref="TimeoutException">the user has taken too long</exception>
-    public void Confirm(string message, Timeout timeout)
+    public void Confirm(string message, Timeout timeout = null)
     {
         throw new TimeoutException();
     }
@@ -76,10 +76,11 @@ public class InputController : MonoBehaviour {
     /// prompt the user to select a tile
     /// </summary>
     /// <param name="timeout" >a timeout for the user to choose in</param>
+    /// <param name="cancelable">should the user be able to cancel the prompt</param>
     /// <exception cref="CancelledException">thrown if the user cancels</exception>
     /// <exception cref="TimeoutException">thrown if the user runs out of time</exception>
     /// <returns>the chosen tile or null if the user times out </returns>
-    public Tile ChooseTile(Timeout timeout)
+    public Tile ChooseTile(Timeout timeout = null, bool cancelable = false)
     {
         return null;
     }
