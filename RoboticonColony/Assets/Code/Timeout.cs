b3a22b698767643cfec518a7f419915549a89617
Robin Stephenson <robin.mascros@gmail.com>
2017-01-14 14:57:15 +0000

    Created a Timeout class to help keep track of phase times

---
+++ RoboticonColony/Assets/Code/Timeout.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Timeout
+{
+    private int timeAllowed;
+    private Stopwatch stopwatch = new Stopwatch();
+
+    /// <summary>
+    /// creates a timout object. use to keep track of how long phases are going
+    /// </summary>
+    /// <param name="time">time in seconds of the timeout</param>
+    public Timeout(int time)
+    {
+        timeAllowed = time;
+    }
+
+    /// <summary>
+    /// start the timer
+    /// </summary>
+    public void Start()
+    {
+        stopwatch.Start();
+    }
+
+    /// <summary>
+    /// get the seconds remaining before a timeout
+    /// </summary>
+    public int SecondsRemaining
+    {
+        get
+        {
+            return Math.Max(timeAllowed - stopwatch.Elapsed.Seconds, 0);
+        }
+    }
+
+    /// <summary>
+    /// check if the timeout has reached its time allowed
+    /// </summary>
+    public bool Finished
+    {
+        get
+        {
+            return timeAllowed - stopwatch.Elapsed.Seconds > 0;
+        }
+    }
+}
