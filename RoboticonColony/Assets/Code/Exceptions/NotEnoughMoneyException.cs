32c54213bf6f8086497beca5940b07022bb780ba
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2017-01-14 16:06:32 +0000

    Created NotEnoughMoneyException and NotEnoughStockException

---
+++ RoboticonColony/Assets/Code/Exceptions/NotEnoughMoneyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class NotEnoughMoneyException : Exception {
+
+    public NotEnoughMoneyException()
+    {
+    }
+
+    public NotEnoughMoneyException(string message) : base(message)
+    {
+    }
+
+    public NotEnoughMoneyException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
---
+++ RoboticonColony/Assets/Code/Exceptions/NotEnoughStockException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class NotEnoughStockException : Exception
+{
+
+    public NotEnoughStockException()
+    {
+    }
+
+    public NotEnoughStockException(string message) : base(message)
+    {
+    }
+
+    public NotEnoughStockException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
