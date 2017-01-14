f79405ed1f1abbfd95d63801116af290c59b6c04
Robin Stephenson <robin.mascros@gmail.com>
2017-01-14 17:20:36 +0000

    Created CancelledException

---
+++ RoboticonColony/Assets/Code/CancelledException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class CancelledException : Exception
+{
+    public CancelledException()
+    {
+    }
+
+    public CancelledException(string message) : base(message)
+    {
+    }
+
+    public CancelledException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
