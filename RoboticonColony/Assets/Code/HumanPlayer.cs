75eee1f3c4cbd844f895bedb62a4b6ea54fa68f6
Robin Stephenson <robin.mascros@gmail.com>
2017-01-14 18:34:22 +0000

    Restructured DoPhaseOne to use the errors from inputcontroller rather than nulls

--- RoboticonColony/Assets/Code/HumanPlayer.cs
+++ RoboticonColony/Assets/Code/HumanPlayer.cs
@@ -24,24 +24,50 @@ public class HumanPlayer : AbstractPlayer
     /// <param name="timeout">a time for which the phase can run</param>
     public override void DoPhaseOne(Timeout timeout)
     {
-        bool? wishestoPurchase = input.PromptBool("Do you wish to purchase a tile?", timeout);
-        if (wishestoPurchase == true)
+        // does the player want to buy a tile?
+        bool wishesToPurchase = false;
+        try
         {
+            wishesToPurchase = input.PromptBool("Do you wish to purchase a tile?", timeout);
+        }
+        catch (TimeoutException)
+        {
+            return;
+        }
+
+        // loop until timeout, cancel, or tile purchased
+        while (wishesToPurchase)
+        {
+            // get the user to choose a tile to buy
             Tile chosenTile;
-            bool tileChosen = false;
-            do
+            try
             {
-                // get the user to choose a tile
                 chosenTile = ChooseTile(timeout);
+            }
+            catch (Exception exception)
+            {
+                if (exception is CancelledException || exception is TimeoutException)
+                {
+                    return;
+                }
+                throw;
+            }
 
-                // check the tile is now owned by anyone and the user has enough money
-                int tileCost = estateAgent.GetPrice(chosenTile);
-                tileChosen = chosenTile.Owner == null && tileCost <= inventory.Money;
-            } while (!tileChosen && timeout.SecondsRemaining > 0);
-
-            if (tileChosen)
+            // try to buy the tile
+            try
             {
                 estateAgent.Buy(chosenTile, inventory);
+                wishesToPurchase = false;
+            }
+            catch (TimeoutException)
+            {
+                return;
+            }
+            catch
+            {
+                // TODO catch a not enough money exception
+                // TODO catch a already owned exception
+                // give the user another go to pick a tile
             }
         }
     }
@@ -82,6 +108,8 @@ public class HumanPlayer : AbstractPlayer
     /// prompt the user to select a tile
     /// </summary>
     /// <param name="timeout" >a timeout for the user to choose in</param>
+    /// <exception cref="CancelledException">thrown if the user cancels</exception>
+    /// <exception cref="TimeoutException">thrown if the user runs out of time</exception>
     /// <returns>the chosen tile or null if the user times out </returns>
     private Tile ChooseTile(Timeout timeout)
     {
