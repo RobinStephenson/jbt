cad4b88ae5592224f4c4d8dd56d4913859c48afa
Robin Stephenson <robin.mascros@gmail.com>
2017-01-16 16:47:02 +0000

    Created placeholder InstallRoboticon method in Tile

---
+++ RoboticonColony/Assets/Code/Exceptions/RoboticonAlreadyInstalledException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class RoboticonAlreadyInstalledException : Exception
+{
+    public RoboticonAlreadyInstalledException()
+    {
+    }
+
+    public RoboticonAlreadyInstalledException(string message) : base(message)
+    {
+    }
+
+    public RoboticonAlreadyInstalledException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
--- RoboticonColony/Assets/Code/Tile.cs
+++ RoboticonColony/Assets/Code/Tile.cs
@@ -15,6 +15,18 @@ public class Tile : MonoBehaviour {
     /// <returns>the removed roboticon</returns>
     public Roboticon RemoveRoboticon()
     {
+        // should also set installed tile to null on the roboticon
         throw new InvalidOperationException("No Roboticon installed");
+    }
+
+    /// <summary>
+    /// install a roboticon on this tile
+    /// </summary>
+    /// <exception cref="RoboticonAlreadyInstalledException">A roboticon is already installled</exception>
+    /// <param name="newRoboticon">the roboticon being installed</param>
+    public void InstallRoboticon(Roboticon newRoboticon)
+    {
+        // should also set the installedTile on the roboticon to this one
+        throw new RoboticonAlreadyInstalledException();
     }
 }
