0b9fa8b33e7d7a5d3addf67c268d226f85ef1f54
Robin Stephenson <robin.mascros@gmail.com>
2017-01-16 12:49:29 +0000

    Created placeholder RemoveRoboticon method in Tile

--- RoboticonColony/Assets/Code/Tile.cs
+++ RoboticonColony/Assets/Code/Tile.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Tile : MonoBehaviour {
 
     // A placeholder
     public AbstractPlayer Owner;
+    public Roboticon InstalledRoboticon;
 
+    /// <summary>
+    /// remove the roboticon installed on this tile
+    /// </summary>
+    /// <exception cref="InvalidOperationException">there is no roboticon installed</exception>
+    /// <returns>the removed roboticon</returns>
+    public Roboticon RemoveRoboticon()
+    {
+        throw new InvalidOperationException("No Roboticon installed");
+    }
 }
