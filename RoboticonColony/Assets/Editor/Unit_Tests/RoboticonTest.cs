48c7d07f1df750072d866ac86b7d85a24df48823
tc975 <tc975@york.ac.uk>
2017-01-23 17:42:52 +0000

    Added Roboticon and Roboticon customisation tests

--- RoboticonColony/Assets/Editor/Unit_Tests/RoboticonTest.cs
+++ RoboticonColony/Assets/Editor/Unit_Tests/RoboticonTest.cs
@@ -7,4 +7,12 @@ using System.Collections.Generic;
 
 public class RoboticonTest
 {
+    [Test]
+    public void BaseRoboticonProductionMultiplierTest()
+    {
+        Roboticon r = new Roboticon(new Tile(4, 6, 4, 5));
+
+        Assert.AreEqual(1, r.ProductionMultiplier(ItemType.Ore));
+        Assert.AreEqual(1, r.ProductionMultiplier(ItemType.Power));
+    }
 }
