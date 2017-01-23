1f630880a47ea06c714f4b35d83edc5d1947af92
tc975 <tc975@york.ac.uk>
2017-01-23 16:23:21 +0000

    Added InstalRoboticonNoRoboticonsTest

--- RoboticonColony/Assets/Editor/Unit_Tests/TileTest.cs
+++ RoboticonColony/Assets/Editor/Unit_Tests/TileTest.cs
@@ -39,7 +39,7 @@ public class TileTest
     [Test]
     public void SuccessfulInstallRoboticon()
     {
-        //Creates a new tile and assigns a roboticon to the tile
+        //Creates a new tile and player and assigns a roboticon to the tile
         Tile tile = new Tile(2, 4, 3, 2);
         Inventory playerInv = new Inventory(100, 10, 10, 10);
         HumanPlayer player = new HumanPlayer("P1", playerInv, new Market(2, 2, 2, 2, 2, 2), new Sprite());
@@ -52,7 +52,7 @@ public class TileTest
     [Test]
     public void InstallRoboticonMultipleRoboticonsTest()
     {
-        //Creates a new tile and assigns a roboticon to the tile
+        //Creates a new tile and player and assigns a roboticon to the tile
         Tile tile = new Tile(2, 4, 3, 2);
         Inventory playerInv = new Inventory(100, 10, 10, 10);
         HumanPlayer player = new HumanPlayer("P1", playerInv, new Market(2, 2, 2, 2, 2, 2), new Sprite());
@@ -60,6 +60,18 @@ public class TileTest
 
         //Checks that a roboticon can't be installed if there is already one installed on the tile
         Assert.Throws<RoboticonAlreadyInstalledException>(() => tile.InstallRoboticon(player));
+    }
+
+    [Test]
+    public void InstallRoboticonNoRoboticonsTest()
+    {
+        //Creates a new tile and player with no roboticons in their inventory
+        Tile tile = new Tile(2, 4, 3, 2);
+        Inventory playerInv = new Inventory(100, 10, 10, 0);
+        HumanPlayer player = new HumanPlayer("P1", playerInv, new Market(2, 2, 2, 2, 2, 2), new Sprite());
+
+        //Checks that a roboticon is not installed if the player has no roboticons
+        Assert.Throws<ArgumentOutOfRangeException>(() => tile.InstallRoboticon(player));
     }
 
 }
