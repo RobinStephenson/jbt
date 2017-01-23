2e29bc2c9d65e67397f632b8652ecd237dd7e18b
tc975 <tc975@york.ac.uk>
2017-01-23 15:00:28 +0000

    fixed comments for ProduceTest

--- RoboticonColony/Assets/Editor/Unit_Tests/TileTest.cs
+++ RoboticonColony/Assets/Editor/Unit_Tests/TileTest.cs
@@ -24,14 +24,15 @@ public class TileTest
     [Test]    
     public void ProduceTest()
     {
+        //Create tile and assign roboticon to tile
         Tile tile = new Tile(2, 4, 3, 2);
         Inventory playerInv = new Inventory(100, 10, 10, 10);
         HumanPlayer player = new HumanPlayer("P1", playerInv, new Market(2, 2, 2, 2, 2, 2), new Sprite());
         tile.InstallRoboticon(player);
         Dictionary<ItemType, int> production = tile.Produce();
-
+       
+        //Check if the correct values for ore and power are returned
         Assert.AreEqual(production[ItemType.Ore], 3);
         Assert.AreEqual(production[ItemType.Power], 2);
-        // assert it gives the expected production
     }
 }
