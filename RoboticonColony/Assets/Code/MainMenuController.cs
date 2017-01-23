f3bed938592d984c337956adb4e57ffb130cd364
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2017-01-23 13:21:21 +0000

    Finished Main menu

--- RoboticonColony/Assets/Code/MainMenuController.cs
+++ RoboticonColony/Assets/Code/MainMenuController.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static string PlayerOneName;
+    public static string PlayerTwoName;
+
+    public Text PlayerOneNameText;
+    public Text PlayerTwoNameText;
+
+    public void StartGameClicked()
+    {
+        if (PlayerOneNameText.text.Length == 0)
+        {
+            PlayerOneName = "Player One";
+        }
+        else
+        {
+            PlayerOneName = PlayerOneNameText.text;
+        }
+
+        if (PlayerTwoNameText.text.Length == 0)
+        {
+            PlayerTwoName = "Player Two";
+        }
+        else
+        {
+            PlayerTwoName = PlayerTwoNameText.text;
+        }
+
+        SceneManager.LoadScene(1);
+    }
 }
